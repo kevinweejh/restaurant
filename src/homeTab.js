dc05697535eef35fca51d1809bb61b56ec080c00
Wee Jian Hui Kevin <kevin@Wees-MacBook-Air.local>
2024-01-17 16:23:24 +0800

    style: remove black background

--- src/homeTab.js
+++ src/homeTab.js
@@ -3,10 +3,10 @@ import { createElement } from './utils.js';
 export default () => {
     const homeTab = () => {
         const homeTab = createElement('div', ['flex', 'flex-col', 'mb-6'], '', 'homeTab');
-        const homeHeadline = createElement('h1', ['text-2xl', 'text-white', 'p-6', 'text-center', 'bg-black/50', 'w-max', 'flex', 'mx-auto'], 'Restaurant de la Victoire');
+        const homeHeadline = createElement('h1', ['text-2xl', 'text-white', 'p-6', 'text-center', 'w-max', 'flex', 'mx-auto'], 'Restaurant de la Victoire');
         homeTab.appendChild(homeHeadline);
 
-        const homeSubtext = createElement('p', ['text-lg', 'text-white', 'text-center', 'bg-black/50', 'w-max', 'flex', 'mx-auto'], 'Best restaurant from here to Mars and back');
+        const homeSubtext = createElement('p', ['text-lg', 'text-white', 'text-center', 'w-max', 'flex', 'mx-auto'], 'Best restaurant from here to Mars and back');
         homeTab.appendChild(homeSubtext);
 
         const homeOpeningHours = createElement('div', ['p-6', 'text-2xl', 'font-semibold', 'text-white', 'text-center'],'Opening Hours','openingHours')
